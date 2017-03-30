8873d225527912283db3e0ef184dd11514f48881
aashwin <aashwin.varshney@gmail.com>
2017-03-31 00:27:34 +1100

    integrating ass2

---
+++ Recogniser/Recogniser.java
@@ -0,0 +1,623 @@
+/***
+ * *
+ * * Recogniser.java            
+ * *
+ ***/
+
+/* At this stage, this parser accepts a subset of VC defined	by
+ * the following grammar. 
+ *
+ * You need to modify the supplied parsing methods (if necessary) and 
+ * add the missing ones to obtain a parser for the VC language.
+ *
+ * (17---March---2017)
+
+ program       -> func-decl
+
+// declaration
+
+func-decl     -> void identifier "(" ")" compound-stmt
+
+identifier    -> ID
+
+// statements 
+compound-stmt -> "{" stmt* "}" 
+stmt          -> continue-stmt
+|  expr-stmt
+continue-stmt -> continue ";"
+expr-stmt     -> expr? ";"
+
+// expressions 
+expr                -> assignment-expr
+assignment-expr     -> additive-expr
+additive-expr       -> multiplicative-expr
+|  additive-expr "+" multiplicative-expr
+multiplicative-expr -> unary-expr
+|  multiplicative-expr "*" unary-expr
+unary-expr          -> "-" unary-expr
+|  primary-expr
+
+primary-expr        -> identifier
+|  INTLITERAL
+| "(" expr ")"
+*/
+
+package VC.Recogniser;
+
+import VC.Scanner.Scanner;
+import VC.Scanner.SourcePosition;
+import VC.Scanner.Token;
+import static VC.Scanner.Token.*;
+import VC.ErrorReporter;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
+
+public class Recogniser {
+	private static final Map<String, Integer[]> first;
+	static {
+		first = new HashMap<String, Integer[]>();
+		first.put("program", new Integer[]{VOID, BOOLEAN, INT, FLOAT});
+		first.put("decl", new Integer[]{LPAREN, LBRACKET, EQ, COMMA});
+		first.put("func-decl", new Integer[]{VOID, BOOLEAN, INT, FLOAT});
+		first.put("func-decl-new", new Integer[]{LPAREN});
+		first.put("var-decl", new Integer[]{VOID, BOOLEAN, INT, FLOAT});
+		first.put("var-decl-new", new Integer[]{LBRACKET, EQ, COMMA});
+		first.put("init-declarator-list", new Integer[]{ID});
+		first.put("init-declarator", new Integer[]{ID});
+		first.put("declarator", new Integer[]{ID});
+		first.put("initialiser", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL, LCURLY});
+		first.put("type", new Integer[]{VOID, BOOLEAN, INT, FLOAT});
+		first.put("identifier", new Integer[]{ID});
+		first.put("compound-stmt", new Integer[]{LCURLY});
+		first.put("stmt", new Integer[]{LCURLY, IF, FOR, WHILE, BREAK, CONTINUE, RETURN, PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL,
+			BOOLEANLITERAL, STRINGLITERAL});
+		first.put("if-stmt", new Integer[]{IF});
+		first.put("for-stmt", new Integer[]{FOR});
+		first.put("while-stmt", new Integer[]{WHILE});
+		first.put("break-stmt", new Integer[]{BREAK});
+		first.put("continue-stmt", new Integer[]{CONTINUE});
+		first.put("return-stmt", new Integer[]{RETURN});
+		first.put("expr-stmt", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL, COMMA});
+		first.put("expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("assignment-expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("cond-or-expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("cond-and-expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("equality-expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("rel-expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("additive-expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("multiplicative-expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("primary-expr", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("para-list", new Integer[]{LPAREN});
+		first.put("proper-para-list", new Integer[]{VOID, BOOLEAN, INT, FLOAT});
+		first.put("para-decl", new Integer[]{VOID, BOOLEAN, INT, FLOAT});
+		first.put("arg-list", new Integer[]{LPAREN});
+		first.put("proper-arg-list", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+		first.put("arg", new Integer[]{PLUS, MINUS, NOT, ID, LPAREN, INTLITERAL, FLOATLITERAL, BOOLEANLITERAL, STRINGLITERAL});
+	}
+	private Scanner scanner;
+	private ErrorReporter errorReporter;
+	private Token currentToken;
+
+	public Recogniser (Scanner lexer, ErrorReporter reporter) {
+		scanner = lexer;
+		errorReporter = reporter;
+
+		currentToken = scanner.getToken();
+	}
+
+	// match checks to see f the current token matches tokenExpected.
+	// If so, fetches the next token.
+	// If not, reports a syntactic error.
+
+	void match(int tokenExpected) throws SyntaxError {
+		if (currentToken.kind == tokenExpected) {
+			currentToken = scanner.getToken();
+		} else {
+			syntacticError("\"%\" expected here", Token.spell(tokenExpected));
+		}
+	}
+
+	// accepts the current token and fetches the next
+	void accept() {
+		currentToken = scanner.getToken();
+	}
+
+	void syntacticError(String messageTemplate, String tokenQuoted) throws SyntaxError {
+		SourcePosition pos = currentToken.position;
+		errorReporter.reportError(messageTemplate, tokenQuoted, pos);
+		throw(new SyntaxError());
+	}
+
+	boolean currentInFirst(String s) {
+		return Arrays.asList(first.get(s)).contains(currentToken.kind);
+	}
+
+
+	// ========================== PROGRAMS ========================
+
+	public void parseProgram() {
+
+		try {
+			// TODO
+			parseType();
+			parseIdent();
+			while (currentInFirst("decl")) {
+				parseDecl();
+			}
+			if (currentToken.kind != Token.EOF) {
+				syntacticError("\"%\" wrong result type for a function", currentToken.spelling);
+			}
+		}
+		catch (SyntaxError s) {  }
+	}
+
+	// ========================== DECLARATIONS ========================
+
+	void parseDecl() throws SyntaxError {
+		if (currentInFirst("func-decl-new")) {
+			// TODO
+			parseFuncDeclNew();
+		} else {
+			//TODO
+			parseVarDeclNew();
+		}
+	}
+
+	void parseFuncDeclNew() throws SyntaxError {
+		// TODO
+		parseParaList();
+		// TODO
+		parseCompoundStmt();
+	}
+
+	void parseVarDecl() throws SyntaxError {
+		parseType();
+		parseInitDeclaratorList();
+		match(Token.SEMICOLON);
+	}
+
+	void parseVarDeclNew() throws SyntaxError {
+		switch(currentToken.kind) {
+		case Token.LBRACKET:
+			match(Token.LBRACKET);
+			if (currentToken.kind == Token.INTLITERAL) {
+				match(Token.INTLITERAL);
+			}
+			match(Token.RBRACKET);
+			break;
+		case Token.EQ:
+			match(Token.EQ);
+			// TODO
+			parseIdent();
+			break;
+		case Token.COMMA:
+			while (currentToken.kind == Token.COMMA) {
+				match(Token.COMMA);
+				// TODO
+				parseInitDeclarator();
+			}
+		}
+	}
+
+	void parseFuncDecl() throws SyntaxError {
+
+		parseIdent();
+		parseParaList();
+		parseCompoundStmt();
+	}
+
+	void parseInitDeclarator() throws SyntaxError {
+		parseDeclarator();
+		if (currentToken.kind == Token.EQ) {
+			match(Token.EQ);
+			// TODO
+			parseInitialiser();
+		}
+	}
+
+	void parseInitDeclaratorList() throws SyntaxError {
+		parseInitDeclarator();
+		while (currentToken.kind == Token.COMMA) {
+			match(Token.COMMA);
+			parseInitDeclarator();
+		}
+	}
+
+	void parseDeclarator() throws SyntaxError {
+		parseIdent();
+		if (currentToken.kind == Token.LBRACKET) {
+			match(Token.LBRACKET);
+			if (currentToken.kind == Token.INTLITERAL) {
+				match(Token.INTLITERAL);
+			}
+			match(Token.RBRACKET);
+		}
+	}
+
+	void parseInitialiser() throws SyntaxError {
+		if (currentToken.kind == Token.LCURLY) {
+			match(Token.LCURLY);
+			// TODO
+			parseExpr();
+			while (currentToken.kind == Token.COMMA) {
+				match(Token.COMMA);
+				parseExpr();
+			}
+			match(Token.RCURLY);
+		} else {
+			parseExpr();
+		}
+	}
+	
+	// ========================== PRIMITIVE TYPES========================
+	void parseType() throws SyntaxError { 
+		switch(currentToken.kind) {
+		case Token.VOID:
+			match(Token.VOID);
+			break;
+		case Token.BOOLEAN:
+			match(Token.BOOLEAN);
+			break;
+		case Token.INT:
+			match(Token.INT);
+			break;
+		case Token.FLOAT:
+			match(Token.FLOAT);
+			break;
+		default:
+			syntacticError("expecting a type", currentToken.spelling);
+		}
+	}
+
+
+	// ======================= STATEMENTS ==============================
+
+
+	void parseCompoundStmt() throws SyntaxError {
+
+		match(Token.LCURLY);
+		while (currentInFirst("var-decl")) {
+			//TODO	
+			parseVarDecl();
+
+		}
+		parseStmtList();
+		match(Token.RCURLY);
+	}
+
+	// Here, a new nontermial has been introduced to define { stmt } *
+	void parseStmtList() throws SyntaxError {
+		while (currentInFirst("stmt")) {
+			parseStmt();
+		}
+	}
+
+	void parseStmt() throws SyntaxError {
+		if (currentInFirst("compound-stmt")) {
+			parseCompoundStmt();
+		} else if (currentInFirst("if-stmt")) {
+			// TODO
+			parseIfStmt();
+		} else if (currentInFirst("for-stmt")) {
+			//TODO
+			parseForStmt();
+		} else if (currentInFirst("while-stmt")) {
+			//TODO
+			parseWhileStmt();
+		} else if (currentInFirst("continue-stmt")) {
+			//TODO
+			parseContinueStmt();
+		} else if (currentInFirst("break-stmt")) {
+			//TODO
+			parseBreakStmt();
+		} else if (currentInFirst("return-stmt")) {
+			//TODO
+			parseReturnStmt();
+		} else {
+			parseExprStmt();
+		}
+	}
+
+	void parseIfStmt() throws SyntaxError {
+		match(Token.IF);
+		match(Token.LCURLY);
+		parseExpr();
+		match(Token.RCURLY);
+		parseStmt();
+		if (currentToken.kind == Token.ELSE) {
+			match(Token.ELSE);
+			parseStmt();
+		}
+	}
+
+	void parseForStmt() throws SyntaxError {
+		match(Token.FOR);
+		match(Token.LPAREN);
+		if (currentInFirst("expr")) {
+			parseExpr();
+		}
+		match(Token.SEMICOLON);
+		if (currentInFirst("expr")) {
+			parseExpr();
+		}
+		match(Token.SEMICOLON);
+		if (currentInFirst("expr")) {
+			parseExpr();
+		}
+		match(Token.RPAREN);
+		parseStmt(); 
+	}
+
+	void parseWhileStmt() throws SyntaxError {
+		match(Token.WHILE);
+		match(Token.LPAREN);
+		//TODO
+		parseExpr();
+		match(Token.RPAREN);
+		parseStmt();
+
+	}
+
+	void parseBreakStmt() throws SyntaxError {
+		match(Token.BREAK);
+		match(Token.SEMICOLON);
+	}
+
+	void parseContinueStmt() throws SyntaxError {
+		match(Token.CONTINUE);
+		match(Token.SEMICOLON);
+	}
+
+	void parseReturnStmt() throws SyntaxError {
+		match(Token.RETURN);
+		if (currentInFirst("expr")) {
+			parseExpr();
+		}
+		match(Token.SEMICOLON);
+	}
+
+	void parseExprStmt() throws SyntaxError {
+
+		if (currentInFirst("expr")) {
+			parseExpr();
+		}
+		match(Token.SEMICOLON);
+	}
+
+
+	// ======================= IDENTIFIERS ======================
+
+	// Call parseIdent rather than match(Token.ID). 
+	// In Assignment 3, an Identifier node will be constructed in here.
+
+
+	void parseIdent() throws SyntaxError {
+
+		if (currentToken.kind == Token.ID) {
+			currentToken = scanner.getToken();
+		} else 
+			syntacticError("identifier expected here", "");
+	}
+
+	// ======================= OPERATORS ======================
+
+	// Call acceptOperator rather than accept(). 
+	// In Assignment 3, an Operator Node will be constructed in here.
+
+	void acceptOperator() throws SyntaxError {
+
+		currentToken = scanner.getToken();
+	}
+
+
+	// ======================= EXPRESSIONS ======================
+
+	void parseExpr() throws SyntaxError {
+		parseAssignExpr();
+	}
+
+
+	void parseAssignExpr() throws SyntaxError {
+		parseCondOrExpr();
+	}
+	
+	void parseCondOrExpr() throws SyntaxError {
+		parseCondAndExpr();
+		while (currentToken.kind == Token.EQ) {
+			match(Token.EQ);
+			parseCondAndExpr();
+		}
+	}
+
+	void parseCondAndExpr() throws SyntaxError {
+		parseEqualityExpr();
+		while (currentToken.kind == Token.ANDAND) {
+			match(Token.ANDAND);
+			parseEqualityExpr();
+		}
+	}
+
+	void parseEqualityExpr() throws SyntaxError {
+		parseRelExpr();
+		while (currentToken.kind == Token.EQEQ || currentToken.kind == Token.NOTEQ) {
+			if (currentToken.kind == Token.EQEQ) {
+				match(Token.EQEQ);
+			} else {
+				match(Token.NOTEQ);
+			}
+			parseRelExpr();	
+		}
+	}
+
+	void parseRelExpr() throws SyntaxError {
+		parseAdditiveExpr();
+		switch(currentToken.kind) {
+		case Token.LT:
+			match(Token.LT);
+			parseAdditiveExpr();
+			break;
+		case Token.LTEQ:
+			match(Token.LTEQ);
+			parseAdditiveExpr();
+			break;
+		case Token.GT:
+			match(Token.GT);
+			parseAdditiveExpr();
+			break;
+		default:
+			match(Token.GTEQ);
+			parseAdditiveExpr();
+			break;
+		}
+
+	}
+
+	void parseAdditiveExpr() throws SyntaxError {
+		parseMultiplicativeExpr();
+		while (currentToken.kind == Token.PLUS || currentToken.kind == Token.MINUS) {
+			acceptOperator();
+			parseMultiplicativeExpr();
+		}
+	}
+
+	void parseMultiplicativeExpr() throws SyntaxError {
+		parseUnaryExpr();
+		while (currentToken.kind == Token.MULT || currentToken.kind == Token.DIV) {
+			acceptOperator();
+			parseUnaryExpr();
+		}
+	}
+
+
+	void parseUnaryExpr() throws SyntaxError {
+		switch (currentToken.kind) {
+			case Token.PLUS:
+			case Token.MINUS:
+			case Token.NOT:
+				acceptOperator();
+				parseUnaryExpr();
+				break;
+			default:
+				parsePrimaryExpr();
+				break;
+
+		}
+	}
+
+	void parsePrimaryExpr() throws SyntaxError {
+
+		switch (currentToken.kind) {
+			case Token.ID:
+				parseIdent();
+				break;
+			case Token.LPAREN:
+				{
+					accept();
+					parseExpr();
+					match(Token.RPAREN);
+				}
+				break;
+			case Token.INTLITERAL:
+				parseIntLiteral();
+				break;
+			case Token.FLOATLITERAL:
+				parseFloatLiteral();
+				break;
+			case Token.BOOLEANLITERAL:
+				parseBooleanLiteral();
+				break;
+			case Token.STRINGLITERAL:
+				parseStringLiteral();
+				break;
+			default:
+				syntacticError("illegal parimary expression", currentToken.spelling);
+
+		}
+	}
+	
+
+	// ========================== PARAMETERS ========================
+	
+	void parseParaList() throws SyntaxError {
+		match(Token.LPAREN);
+		if (currentInFirst("proper-para-list")) {
+			parseProperParaList();
+		}
+		match(Token.RPAREN);
+	}
+
+	void parseProperParaList() throws SyntaxError {
+		// TODO
+		parseParaDecl();
+		if (currentToken.kind == Token.COMMA) {
+			match(Token.COMMA);
+			parseParaDecl();
+		}
+	}
+
+	void parseParaDecl() throws SyntaxError {
+		parseType();
+		// TODO
+		parseDeclarator();
+	}
+
+	void parseArgList() throws SyntaxError {
+		match(Token.LPAREN);
+		if (currentInFirst("proper-arg-list")) {
+			parseProperArgList();
+		}
+		match(Token.RPAREN);
+	}
+
+	void parseProperArgList() throws SyntaxError {
+		parseArg();
+		while (currentToken.kind == Token.COMMA) {
+			match(Token.COMMA);
+			parseArg();
+		}
+	}
+
+	void parseArg() throws SyntaxError {
+		parseExpr();
+	}
+
+
+	// ========================== LITERALS ========================
+
+	// Call these methods rather than accept().  In Assignment 3, 
+	// literal AST nodes will be constructed inside these methods. 
+
+	void parseIntLiteral() throws SyntaxError {
+
+		if (currentToken.kind == Token.INTLITERAL) {
+			currentToken = scanner.getToken();
+		} else 
+			syntacticError("integer literal expected here", "");
+	}
+
+	void parseFloatLiteral() throws SyntaxError {
+
+		if (currentToken.kind == Token.FLOATLITERAL) {
+			currentToken = scanner.getToken();
+		} else 
+			syntacticError("float literal expected here", "");
+	}
+
+	void parseBooleanLiteral() throws SyntaxError {
+
+		if (currentToken.kind == Token.BOOLEANLITERAL) {
+			currentToken = scanner.getToken();
+		} else 
+			syntacticError("boolean literal expected here", "");
+	}
+
+	void parseStringLiteral() throws SyntaxError {
+
+		if (currentToken.kind == Token.STRINGLITERAL) {
+			currentToken = scanner.getToken();
+		} else 
+			syntacticError("string literal expected here", "");
+	}
+}
