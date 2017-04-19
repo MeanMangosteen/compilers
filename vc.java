088373191e998a5f7f50b6be673b13a453dc6c8f
aashwin <aashwin.varshney@gmail.com>
2017-04-20 01:32:28 +1000

    vc main file added

--- vc.java
+++ vc.java
@@ -1,10 +1,4 @@
 /*
- * vc.java           
- *
- * F O R     A S S I G N M E N T    2
- * 
- * 
- * Jingling Xue, CSE, UNSW, Sydney NSW 2052, Australia.
  * 
  */
 
@@ -12,22 +6,70 @@ package VC;
 
 import VC.Scanner.Scanner;
 import VC.Scanner.SourceFile;
-import VC.Recogniser.Recogniser;
+import VC.Parser.Parser;
+import VC.TreeDrawer.Drawer;
+import VC.TreePrinter.Printer;
+import VC.UnParser.UnParser;
+import VC.ASTs.AST;
 
 public class vc {
 
     private static Scanner scanner;
     private static ErrorReporter reporter;
-    private static Recogniser recogniser;
+    private static Parser parser;
+    private static Drawer drawer;
+    private static Printer printer;
+    private static UnParser unparser;
 
+    private static int drawingAST = 0;
     private static String inputFilename; 
+    private static String VCFilename = null;
+    private static String ASTFilename = null; 
+
+    private static AST theAST;
+
+    private static void cmdLineOptions() {
+      System.out.println("Usage: java VC.vc [-options] filename");
+      System.out.println();
+      System.out.println("where options include:");
+      System.out.println("	-ast 		    display the AST (without SourcePosition)");
+      System.out.println("	-astp 		    display the AST (with SourcePosition)");
+      System.out.println("	-t file             print the AST into <file>"); 
+      System.out.println("	-u file  	    unparse the AST into <file>"); 
+      System.exit(1);
+    }
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-          System.out.println("Usage: java VC.vc filename\n");
-          System.exit(1); 
-        } else
-           inputFilename = args[0];
+        int i = 0;
+        String arg;
+
+        while (i < args.length && args[i].startsWith("-")) {
+          arg = args[i++];
+          
+          if (arg.equals("-ast"))
+            drawingAST = 1;
+          else if (arg.equals("-astp"))
+            drawingAST = 2;
+          else if (arg.equals("-u")) {
+            if (i < args.length)
+              VCFilename = args[i++];
+            else {
+              System.out.println("[# vc #]: invalid option " + arg);
+              cmdLineOptions();
+            }
+          } else if (arg.equals("-t")) {
+            if (i < args.length)
+              ASTFilename = args[i++];
+            else {
+              System.out.println("[# vc #]: invalid option " + arg);
+              cmdLineOptions();
+            }
+          }
+        }
+        if (i == args.length)
+           cmdLineOptions();
+        else
+           inputFilename = args[i];
 
         System.out.println("======= The VC compiler =======");
 
@@ -35,13 +77,37 @@ public class vc {
 
         reporter = new ErrorReporter();
         scanner  = new Scanner(source, reporter);
-        recogniser = new Recogniser(scanner, reporter);
+        parser   = new Parser(scanner, reporter);
 
-        recogniser.parseProgram();
+        if (ASTFilename == null)
+          ASTFilename = inputFilename + "t";
+        printer = new Printer(ASTFilename);
 
-        if (reporter.numErrors == 0)
+        if (VCFilename == null)
+          VCFilename = inputFilename + "u";
+        unparser = new UnParser(VCFilename);
+
+        try {
+        theAST = parser.parseProgram();
+        }
+        catch (Exception e) { 
+          System.out.println("PANIC:");
+	  e.printStackTrace(); 
+          System.exit(1);
+        }
+
+        if (reporter.numErrors == 0) {
            System.out.println ("Compilation was successful.");
-        else
+ 	   drawer   = new Drawer();
+           if (drawingAST == 2)
+             drawer.enableDebugging(); // show SourcePosition
+           if (drawingAST != 0)
+             drawer.draw(theAST); // draw the AST
+
+           printer.print(theAST);
+
+           unparser.unparse(theAST);
+        } else
            System.out.println ("Compilation was unsuccessful.");
     }
 }
